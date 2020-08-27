03d797525f8696d5a898fd3fb44071e0925eaf6d
Matheus De Paula Teixeira <matheus.teixeira.mt1@vcimentos.com>
2020-08-27 20:26:18 -0300

    Ajuste no Carrossel

--- components/Carousel/WithVideo/index.js
+++ components/Carousel/WithVideo/index.js
@@ -5,12 +5,6 @@ import VideoCard from "./components/VideoCard";
 import { VideoCardGroupContainer, Title, ExtraLink } from './styles'
 import './SliderItem.module.css';
 
-const responsive = {
-  doesntmatter: {
-    breakpoint: { max: 3000, min: 0 },
-    items: 1
-  }
-};
 
 function CarrosselComVideo ({ ignoreFirstVideo, category }) {
 
@@ -18,6 +12,33 @@ function CarrosselComVideo ({ ignoreFirstVideo, category }) {
   const categoryColor = category.cor;
   const categoryExtraLink = category.link_extra;
   const videos = category.videos;
+
+  const CustomRightArrow = ({ onClick, ...rest }) => {
+    const {
+      onMove,
+      carouselState: { currentSlide, deviceType } 
+    } = rest;
+    // onMove means if dragging or swiping in progress.
+    return <button onClick={() => onClick()} />;
+  };
+
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+      slidesToSlide: 3 // optional, default to 1.
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+      slidesToSlide: 2 // optional, default to 1.
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      slidesToSlide: 1 // optional, default to 1.
+    }
+  };
 
   return (
     <section>
@@ -34,7 +55,23 @@ function CarrosselComVideo ({ ignoreFirstVideo, category }) {
           }
         </>
       )}
-      <Carousel swipeable={false} draggable={false} responsive={responsive}>
+      <Carousel
+        customRightArrow={<CustomRightArrow />}
+        swipeable={false}
+        draggable={false}
+        showDots={true}
+        responsive={responsive}
+        //ssr={true} // means to render carousel on server-side.
+        infinite={true}
+        autoPlaySpeed={1000}
+        keyBoardControl={true}
+        customTransition="all .5"
+        transitionDuration={500}
+        containerClass="carousel-container"
+        removeArrowOnDeviceType={["tablet", "mobile"]}
+        dotListClass="custom-dot-list-style"
+        itemClass="carousel-item-padding-40-px"
+      >
         {videos.map((video, index) => {
           if (ignoreFirstVideo && index === 0) {
             return null;
